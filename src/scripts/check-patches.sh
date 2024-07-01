7b3cd05b339433cd5a10800f2690fca491db09b0
jackwrfuller <jack.fuller101@gmail.com>
2024-07-01 12:44:43 +1000

    fix typo

--- src/scripts/check-patches.sh
+++ src/scripts/check-patches.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 defined_patches=$(jq '.extra.patches | length' /app/composer.json)
-number_patches_applied=$(grep -c "patches_applied "/app/vendor/composer/installed.json)
+number_patches_applied=$(grep -c "patches_applied" /app/vendor/composer/installed.json)
 if [[ "$defined_patches" -ne "$number_patches_applied" ]]; then
 	echo "The number of patches applied does not match the number defined in the composer.json file"
 	exit 1
